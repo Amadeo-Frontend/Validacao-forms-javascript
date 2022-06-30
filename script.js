5a741f41a396e6f9d1c62146e6d91f90f4b34bd8
Amadeo Bon <amadeosph@outlook.com>
2022-06-30 11:10:34 -0300

    validação

--- script.js
+++ script.js
@@ -1,69 +1,5 @@
 
-function calcularMedia(notas) {
-  let soma = 0;
-  for (c = 0; c < notas.length; c++) {
-    soma += notas[c];
-  }
 
-  media = soma / notas.length;
-
-  return media;
-}
-
-let media; // escopo global
-
-function aprovacao(notas) {
-  let media = calcularMedia(notas); // escopo da função
-
-  let condicao = media >= 8 ? "aprovado" : "reprovado";
-
-  return "Média: " + media + " - Resultado: " + condicao;
-}
-
-// Função Recursivas
-
-function contagemRegressiva(numero) {
-  console.log(numero);
-
-  let proximoNumero = numero - 1;
-
-  if (proximoNumero > 0) contagemRegressiva(proximoNumero);
-}
-
-// contagemRegressiva(50);
-
-/*
- * Formulário envio de dados para cálculo da média
- */
-const formulario1 = document.getElementById("formulario-01");
-
-if (formulario1)
-  formulario1.addEventListener("submit", function (evento) {
-    evento.preventDefault();
-    evento.stopPropagation();
-
-    if (this.getAttribute("class").match(/erro/)) {
-      return false;
-    }
-
-    let dados = new FormData(this);
-
-    let notas = [];
-
-    for (let key of dados.keys()) {
-      let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0; // é um número
-
-      if (!isNaN(numero)) {
-        notas.push(numero);
-      }
-    }
-
-    console.log(notas);
-
-    texto = aprovacao(notas);
-
-    document.getElementById("resultado").innerHTML = texto;
-  });
 
 function validaCampo(elemento) {
   elemento.addEventListener("focusout", function (event) {
@@ -136,3 +72,6 @@ for (let emFoco of camposNumericos) {
 for (let emFoco of camposEmail) {
   validaEmail(emFoco);
 }
+let textUf = document.querySelector('input.uf')
+let caracterMax = textUf.maxLength
+
