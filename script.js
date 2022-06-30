384ddcbefea3e38ff3c2c7d3776a295b2215572e
Amadeo Bon <amadeosph@outlook.com>
2022-06-30 12:15:19 -0300

    valid

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 function validaCampo(elemento) {
   elemento.addEventListener("focusout", function (event) {
     event.preventDefault();
@@ -23,10 +20,11 @@ function validaCampoNumerico(elemento) {
   elemento.addEventListener("focusout", function (event) {
     event.preventDefault();
 
-    let numero = this.value.match(/^[/d]5-[\d]3/)?this.value.replace(/-/, ""):this.value;
+    let numero = this.value.match(/^[/d]5-[\d]3/)
+      ? this.value.replace(/-/, "")
+      : this.value;
     if (numero != "" && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10) {
       document.querySelector(".mensagem").innerHTML = "";
-      this.classList.remove("erro");
       this.parentNode.classList.remove("erro");
     } else {
       document.querySelector(".mensagem").innerHTML =
@@ -72,6 +70,20 @@ for (let emFoco of camposNumericos) {
 for (let emFoco of camposEmail) {
   validaEmail(emFoco);
 }
-let textUf = document.querySelector('input.uf')
-let caracterMax = textUf.maxLength
+let textUf = document.querySelectorAll("input.uf");
+let caracterMax = textUf.maxLength;
 
+let calcularBtn = document.getElementById("calcular");
+let calcular = false;
+
+calcularBtn.addEventListener("click", () => {
+  if (calcular) {
+    calcularBtn.innerText = "Enviado";
+    calcularBtn.style = "background-color:aqua;";
+    calcular = false;
+  } else {
+    calcularBtn.innerText = "Enviar";
+    calcularBtn.style = "background-color:darkblue;";
+    calcular = true;
+  }
+});
